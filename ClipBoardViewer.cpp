bd75713b0d04eebcc43077bd0e351f4fb08487f2
vectercyg <744662105@qq.com>
2022-07-31 09:42:56 +0800

    main function

---
+++ ClipBoardViewer.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: cuiyonggan
+ * @Date: 2022-07-31 08:46:19
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-07-31 08:53:05
+ */
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPresInstance, PSTR szCmdLine, int iCmdShow) {
+    
+    return ;
+}
